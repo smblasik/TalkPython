b308de88bc68287ec3d0717fc08585b70419ea9c
Scott Blasik <smblasik@users.noreply.github.com>
2017-09-20 12:42:30 -0500

    Added App 4

---
+++ App4/journal.py
@@ -0,0 +1,42 @@
+"""
+This is the Journal Module
+
+"""
+
+import os
+
+
+def load(name):
+    """
+    This method creates and loads a new journal.
+
+    :param name: The base name of the journal to load.
+    :return: a new journal data structure populated with the file data.
+    """
+    data = []
+    filename = get_full_pathname(name)
+
+    if os.path.exists(filename):
+        with open(filename) as fin:
+            for entry in fin.readlines():
+                data.append(entry.rstrip())
+
+    return data
+
+
+def save(name, journal_data):
+    filename = get_full_pathname(name)
+    print(".....Saving to: {} ".format(filename))
+
+    with open(filename, 'w') as fout:
+        for entry in journal_data:
+            fout.write(entry + '\n')
+
+
+def get_full_pathname(name):
+    filename = os.path.abspath(os.path.join('.', 'journals/', name + '.jrl'))
+    return filename
+
+
+def add_entry(text, journal_data):
+    journal_data.append(text)
---
+++ App4/program.py
@@ -0,0 +1,55 @@
+import journal
+
+
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    pass
+    dash = 30 * "-"
+    print(dash)
+    print('         Journal App')
+    print(dash)
+
+
+def run_event_loop():
+    print('What do you want to do with your Journal? ')
+    cmd = 'Empty'
+    journal_name = 'default'
+    journal_data = journal.load(journal_name)  # list()
+
+    while cmd != 'X' and cmd:
+        cmd = input('[L]ist entries, [A]dd an entry, E[x]it:  ').strip().upper()
+
+        if cmd == 'L':
+            list_entries(journal_data)
+        elif cmd == 'A':
+            add_entry(journal_data)
+        elif cmd != 'X' and cmd:
+            print("Sorry, we don't understand '{}'".format(cmd))
+
+    print('Done, goodbye.')
+    journal.save(journal_name, journal_data)
+
+
+def list_entries(data):
+    print('Your journal entries: ')
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print('* [{}] {}'.format(idx + 1, entry))
+
+
+def add_entry(data):
+    text = input('Type your entry, <enter> to exit: ')
+    journal.add_entry(text, data)
+    # data.append(text)
+
+
+print(__file__)
+print(__name__)
+
+
+if __name__ == '__main__':
+    main()
